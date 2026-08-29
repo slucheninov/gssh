64f203758e1a9ad3cbb3d168fb06275b5047c02b
Slava Lucheninov <slava.lucheninov@tentens.tech>
2026-08-29 14:56:32 +0200

    release: v1.1.8

--- gssh.zsh
+++ gssh.zsh
@@ -2,7 +2,7 @@
 # gssh - GCP IAP SSH helper with fzf support
 # https://github.com/USER/gssh
 
-GSSH_VERSION="1.1.7"
+GSSH_VERSION="1.1.8"
 
 # --- Configuration defaults (override in .zshrc or .env) ---
 : ${GSSH_ZONES:="us-central1-a us-central1-b us-central1-c"}
